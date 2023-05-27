d91a0dc1ea1088e9cb0e32411179ef7cb0c49539
olena zavertiaieva <zavertiaievasky@gmail.com>
2023-05-28 00:03:42 +0200

    add forms control for world map and fix logic for barchart

--- website/scripts/barchart.js
+++ website/scripts/barchart.js
@@ -1,6 +1,6 @@
 let path = '../../data/multiline_data/barchart_Central Europe, Eastern Europe, and Central Asia_Both.CSV';
 sessionStorage.setItem('pathBarchart', path);
-sessionStorage.setItem('barchartRegion', 'Central Europe, Eastern Europe, and Central Asia')
+sessionStorage.setItem('barchartRegion', 'Central Europe, Eastern Europe, and Central Asia');
 sessionStorage.setItem('barchartSex', 'Male');
 
 
@@ -13,10 +13,27 @@ document.getElementById("button-Both").addEventListener("click", handleClick);
 
 function handleClick(event) {
   var buttonValue = event.target.value;  
-  console.log("Clicked button value:", buttonValue);
+  if(event.target.id == 'button-Male'){
+    event.target.classList.add('focused-button');
+    document.getElementById("button-Female").classList.remove('focused-button');
+    document.getElementById("button-Both").classList.remove('focused-button');
+  }
+
+  if(event.target.id == 'button-Female'){
+    event.target.classList.add('focused-button');
+    document.getElementById("button-Male").classList.remove('focused-button');
+    document.getElementById("button-Both").classList.remove('focused-button');
+  }
+  if(event.target.id == 'button-Both'){
+    event.target.classList.add('focused-button');
+    document.getElementById("button-Male").classList.remove('focused-button');
+    document.getElementById("button-Female").classList.remove('focused-button');
+  }
 
   path = '../../data/multiline_data/barchart_'+ sessionStorage.getItem('barchartRegion')  + '_' + buttonValue + '.CSV';
   sessionStorage.setItem('pathBarchart', path);
+  sessionStorage.setItem('barchartSex', buttonValue);
+
 
 }
 
@@ -33,4 +50,6 @@ var dropdown = document.getElementById('regions');
 
       path = '../../data/multiline_data/barchart_'+ selectedValue + '_' + sessionStorage.getItem('barchartSex') + '.CSV';
       sessionStorage.setItem('pathBarchart', path);
+      sessionStorage.setItem('barchartRegion', selectedValue);
+
 });
